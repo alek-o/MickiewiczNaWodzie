8de6451f8877ee166be25c8e87c104e6accc5d68
joasiadominika <joasia.walczak@gmail.com>
2025-06-10 09:45:29 +0200

    cleanup2

--- resources/shaders/skybox/skybox.fs.glsl
+++ resources/shaders/skybox/skybox.fs.glsl
@@ -1,15 +1,3 @@
-//#version 330 core
-//out vec4 FragColor;
-
-//in vec3 texCoords;
-
-//uniform samplerCube skybox;
-
-//void main()
-//{
-//	FragColor = texture(skybox, texCoords);
-//}
-
 #version 330 core
 
 out vec4 FragColor;
