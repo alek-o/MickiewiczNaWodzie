fdf6a70894e65430977f59b3d84a7550cfc9e876
unknown <aleksander.o@tuta.io>
2025-06-10 13:14:40 +0200

    Changed window name

--- src/main.cpp
+++ src/main.cpp
@@ -111,7 +111,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "MickiewiczNaWodzie", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
